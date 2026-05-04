3b62a569d6c0f8c60022a2be97496abacc3050a2
Pthreea <lukas6289@outlook.com>
2026-05-04 17:30:41 +0400

    10

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -3,8 +3,12 @@ package ru.practicum.shareit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 public class ShareItApp {
 	public static void main(String[] args) {
 		SpringApplication.run(ShareItApp.class, args);
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,27 +1,17 @@
 package ru.practicum.shareit.item.model;
 
 import lombok.*;
-import jakarta.persistence.*;
 import ru.practicum.shareit.user.model.User;
 
-
-@Entity
-@Table(name = "items")
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String description;
     private Boolean available;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
     private User owner;
 }
