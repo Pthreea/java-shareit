c81e8a9e11ad0495f9d7b713106ff761fa43d152
Pthreea <lukas6289@outlook.com>
2026-06-19 15:07:56 +0400

    remove import

--- server/src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ server/src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.repository;
 
 import org.springframework.data.domain.Pageable;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
