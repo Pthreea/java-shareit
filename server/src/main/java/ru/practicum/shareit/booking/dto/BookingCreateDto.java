5af5cdc9186a88fb2e97f799e17b713bf360b0f3
Pthreea <lukas6289@outlook.com>
2026-06-20 22:55:31 +0400

    4.2

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
@@ -1,14 +1,13 @@
 package ru.practicum.shareit.booking.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -17,16 +16,16 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BookingCreateDto {
 
-    @NotNull(message = "Start date is required")
-    @FutureOrPresent(message = "Start date must be in present or future")
+    @NotNull(message = "Item ID cannot be null")
+    private Long itemId;
+
+    @NotNull(message = "Start date cannot be null")
+    @Future(message = "Start date must be in the future")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime start;
 
-    @NotNull(message = "End date is required")
-    @Future(message = "End date must be in future")
+    @NotNull(message = "End date cannot be null")
+    @Future(message = "End date must be in the future")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime end;
-
-    @NotNull(message = "Item id is required")
-    private Long itemId;
 }
--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -7,9 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.model.BookingStatus;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -20,23 +17,13 @@ public class BookingDto {
 
     private Long id;
 
-    @NotNull(message = "Start date is required")
-    @FutureOrPresent(message = "Start date must be in present or future")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime start;
 
-    @NotNull(message = "End date is required")
-    @Future(message = "End date must be in future")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime end;
 
-    @NotNull(message = "Item id is required")
-    private Long itemId;
-
-    private Long bookerId;
-
     private BookingStatus status;
-
     private ItemDto item;
     private UserDto booker;
 
